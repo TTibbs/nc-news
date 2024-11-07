2cbdfd2582c2dbd0d583a02c79399666e5a5003a
Tibbs <terryjw.97@gmail.com>
2024-11-07 12:34:31 +0000

    Fixed optimised loading

---
+++ src/components/Voting.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { DownVoteButton, UpVoteButton } from "./Buttons";
+import { updateArticleVotes } from "../utils/api";
+
+export default function Voting({ votes, article_id }) {
+  const [clientVotes, setClientVotes] = useState(0);
+  const [isError, setIsError] = useState(false);
+  const handleArticleVotes = (num) => {
+    setClientVotes((currVotes) => {
+      return currVotes + num;
+    });
+    updateArticleVotes(article_id, num)
+      .then(() => {
+        setIsError(false);
+      })
+      .catch((err) => {
+        setClientVotes((currVotes) => {
+          return currVotes - num;
+        });
+        setIsError(true);
+      });
+  };
+
+  return (
+    <>
+      <p>Votes: {votes + clientVotes}</p>
+      {isError ? <p>Oops, an error</p> : null}
+      <UpVoteButton handleArticleVotes={handleArticleVotes} />
+      <DownVoteButton handleArticleVotes={handleArticleVotes} />
+    </>
+  );
+}
