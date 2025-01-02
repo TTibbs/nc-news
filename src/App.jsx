1c14b13b9d020a4d0f6a40a2984687bc1e939928
TTibbs <terryjw.97@gmail.com>
2025-01-02 21:07:11 +0000

    Added vercel site visitor analytics

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Auth from "./components/Auth";
 import { ToastContainer } from "react-toastify";
 import Layout from "./components/Layout";
 import Profile from "./components/Profile";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -106,6 +107,7 @@ function App() {
           }
         />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
