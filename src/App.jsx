6a84843ad65b5088c208939da9f40f331068854c
TTibbs <terryjw.97@gmail.com>
2025-01-02 21:11:04 +0000

    Moved analytics component outside of browser router for 500 error fix on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -15,100 +15,102 @@ import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ToastContainer
-        position="bottom-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <Home />
-            </Layout>
-          }
+    <>
+      <BrowserRouter>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
         />
-        <Route
-          path="/auth"
-          element={
-            <Layout>
-              <Auth />
-            </Layout>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <Layout>
-              <Profile />
-            </Layout>
-          }
-        />
-        <Route
-          path="/articles"
-          element={
-            <Layout>
-              <ArticleList />
-            </Layout>
-          }
-        />
-        <Route
-          path="/articles/:article_id"
-          element={
-            <Layout>
-              <SingleArticle />
-            </Layout>
-          }
-        />
-        <Route
-          path="/topics"
-          element={
-            <Layout>
-              <Topics />
-            </Layout>
-          }
-        />
-        <Route
-          path="/topics/:slug"
-          element={
-            <Layout>
-              <TopicSlug />
-            </Layout>
-          }
-        />
-        <Route
-          path="/post-article"
-          element={
-            <Layout>
-              <PostArticle />
-            </Layout>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <Layout>
-              <NotFound
-                error={{
-                  status: 404,
-                  msg: "The page you are looking for does not exist.",
-                }}
-              />
-            </Layout>
-          }
-        />
-      </Routes>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+          <Route
+            path="/auth"
+            element={
+              <Layout>
+                <Auth />
+              </Layout>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <Layout>
+                <Profile />
+              </Layout>
+            }
+          />
+          <Route
+            path="/articles"
+            element={
+              <Layout>
+                <ArticleList />
+              </Layout>
+            }
+          />
+          <Route
+            path="/articles/:article_id"
+            element={
+              <Layout>
+                <SingleArticle />
+              </Layout>
+            }
+          />
+          <Route
+            path="/topics"
+            element={
+              <Layout>
+                <Topics />
+              </Layout>
+            }
+          />
+          <Route
+            path="/topics/:slug"
+            element={
+              <Layout>
+                <TopicSlug />
+              </Layout>
+            }
+          />
+          <Route
+            path="/post-article"
+            element={
+              <Layout>
+                <PostArticle />
+              </Layout>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <Layout>
+                <NotFound
+                  error={{
+                    status: 404,
+                    msg: "The page you are looking for does not exist.",
+                  }}
+                />
+              </Layout>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
       <Analytics />
-    </BrowserRouter>
+    </>
   );
 }
 
